f51cc970f2a3a98f4798386fd99227ab38ddf5bd
DT <dt.zhangjia@gmail.com>
2017-07-03 09:41:23 +0800

    实现原hello单文件全部功能

--- app/main/views.py
+++ app/main/views.py
@@ -4,18 +4,34 @@ learn flask views
 # pylint: disable=invalid-name, too-few-public-methods
 
 from datetime import datetime
-from flask import Flask, url_for, redirect, request, render_template, session, flash
+from flask import Flask, url_for, redirect, request, render_template, session, flash, current_app
 
 from . import main
 from .forms import NameForm
 from .. import mydb
-#from ..models import User
+from ..email import send_email
+from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     """index"""
     pyform = NameForm()
     if pyform.validate_on_submit():
+        usercheck = User.query.filter_by(username=pyform.indexname.data).first()
+        if usercheck is None:
+            session['exist'] = False
+            newuser = User(username=pyform.indexname.data, role_id=3)#建立新用户记录
+            mydb.session.add(newuser)
+
+            send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', mailuser=newuser)
+        else:
+            session['exist'] = True
+        old_name = session.get('pyname')
+        if old_name is not None and old_name != pyform.indexname.data:
+            flash('name changed.')
+
+        session['pyname'] = pyform.indexname.data
+        pyform.indexname.data = ''
         return redirect(url_for('.index'))
     return render_template('index.html', indexform=pyform,
                            indexname2=session.get('pyname'),
